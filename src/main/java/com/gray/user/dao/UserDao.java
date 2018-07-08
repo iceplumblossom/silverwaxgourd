452bc256315d84035bfcb710b2192d0f5e050c38
iceplumblossom <40381674+iceplumblossom@users.noreply.github.com>
2018-07-08 17:36:19 +0800

    initial project
    github:https://github.com/HuangJunXiong/Spring-SpringMVC-MyBatis-Maven
    run OK(login & register without the email without email notification via QQ email)

---
+++ src/main/java/com/gray/user/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.gray.user.dao;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public interface UserDao<User> extends BaseDao<User> {
+	List<User> selectId(String id);
+	List<User> selectEmail(String email);
+	List<User> queryAll();
+	List<User> selectByName(String username);
+}
